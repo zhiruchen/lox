5510b702958ff4b5221f564fdf0cb4fd95dabe37
zhiruchen <zhiruchen1992@foxmail.com>
2017-06-24 21:41:43 +0800

    scanner, token

---
+++ src/com/interpreter/lox/Lox.java
@@ -0,0 +1,55 @@
+package com.interpreter.lox;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+    static boolean hasError = false;
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.println("jlox [usage]");
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPromt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if(hasError) System.exit(65);
+    }
+
+    private static void runPromt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader buffer = new BufferedReader(input);
+
+        for (;;) {
+            System.out.print("> ");
+            run(buffer.readLine());
+            hasError = false;
+        }
+    }
+
+    private static void run(String source) {
+
+    }
+
+    static void error(int line, String message) {
+        report(line ,"", message);
+    }
+
+    static private void report(int line, String where, String message) {
+        System.err.println(
+                "[line " + line + "] where: " + where + ": " + message
+        );
+        hasError = true;
+    }
+
+}
---
+++ src/com/interpreter/lox/Scanner.java
@@ -0,0 +1,70 @@
+package com.interpreter.lox;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.interpreter.lox.TokenType.*;
+
+
+public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            default:
+                Lox.error(line, "unexpected character");
+                break;
+        }
+    }
+
+    private char advance() {
+        current++;
+        return source.charAt(current-1);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+
+}
